e38090bd3870884aa4234d2f925734d7add44acd
MINSK\Ihar_Mialik <Ihar_mialik@epam.com>
2020-03-17 17:03:55 +0300

    Added unit tests projects and folders reshift

---
+++ FileSystemVisitorTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("FileSystemVisitorTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("EPAM Systems")]
+[assembly: AssemblyProduct("FileSystemVisitorTests")]
+[assembly: AssemblyCopyright("Copyright © EPAM Systems 2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("31d08f85-cacd-446b-810e-794f70eeda68")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
